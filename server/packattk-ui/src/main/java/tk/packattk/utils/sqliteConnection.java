fd46ed97aed3b74d290cadfefc58dfbbff016dad
Evan Arnold <earnold1993@gmail.com>
2015-02-15 23:51:52 -0500

    Moved the database to utils and added a connection class

---
+++ server/packattk-ui/src/main/java/tk/packattk/utils/sqliteConnection.java
@@ -0,0 +1,22 @@
+package tk.packattk.utils;
+
+import java.sql.*;
+
+/**
+* Created by Evan on 2/15/2015.
+*/
+public class sqliteConnection {
+    Connection conn = null;
+    public static Connection dbConnector()
+    {
+        try
+        {
+            Class.forName("org.sqlite.JDBC");
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:purdue.db");
+            return conn;
+        } catch ( Exception e )
+        {
+            return null;
+        }
+    }
+}
