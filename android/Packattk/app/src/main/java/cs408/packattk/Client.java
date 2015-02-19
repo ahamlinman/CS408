2cae240e3198a5013e4384516d97e5e4072d6c73
Alex Hamlin <alexh1995@gmail.com>
2015-02-18 21:28:25 -0500

    Update Android client code
    Update the code in the client to use HTTP and connect to the API
    endpoint rather than working with raw sockets.

--- android/Packattk/app/src/main/java/cs408/packattk/Client.java
+++ android/Packattk/app/src/main/java/cs408/packattk/Client.java
@@ -1,5 +1,12 @@
 package cs408.packattk;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.HttpConnectionParams;
+
 import java.net.*;
 import java.io.IOException;
 import java.io.InputStream;
@@ -8,28 +15,19 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.util.Scanner;
 
 /**
  * Created by Cris on 2/12/2015.
  */
 public class Client {
-    private static final int SERVERPORT = 80; // port?
-    private final static String SERVER = "128.211.207.139";
+    private static final String API_ENDPOINT_URL = "http://data.cs.purdue.edu:7225/api";
+    private static final int CONNECTION_TIMEOUT = 10000; // 10 seconds
 
     private final static String LOGIN = "LOGIN";
     private final static String ADDUSER = "ADDUSER";
-    private final static int MSG_SIZE = 64;
 
     private String username = "";
-
-    private InputStream is;
-    private OutputStream os;
-    private InputStreamReader isr;
-    private OutputStreamWriter osw;
-    private BufferedReader br;
-    private BufferedWriter bw;
-    private int bytesRead;
-    private char[] msg = new char[MSG_SIZE];
 
     public Client(String username) {
         this.username = username;
@@ -37,33 +35,73 @@ public class Client {
 
     /*
     * NOTE - SUPER IMPORTANT!!!
-    * This method HAS to be called on a new thread. Android does not let you do networking on the
+    * These methods MUST be called on a new thread. Android does not let you do networking on the
     * main thread. This method assumes it's being called from a thread other than main and just
     * sets up the connection and I/O objects.
     */
-    private int initSocket() {
+
+    /**
+     * Initialize a new HTTP client with default parameters
+     *
+     * @return The initialized client
+     */
+    private HttpClient initClient() {
+        HttpClient client = new DefaultHttpClient();
+        HttpConnectionParams.setConnectionTimeout(client.getParams(), CONNECTION_TIMEOUT);
+        HttpConnectionParams.setConnectionTimeout(client.getParams(), CONNECTION_TIMEOUT);
+        return client;
+    }
+
+    /**
+     * Create a string from an InputStream
+     *
+     * This uses a clever trick: It tokenizes the string based on the delimiter for
+     * the beginning of a string. Thus, it effectively tokenizes the string into one
+     * long token.
+     *
+     * Based on http://stackoverflow.com/a/5445161
+     *
+     * @param is The input stream
+     * @return A string containing the content of the stream
+     */
+    private String inputStreamToString(InputStream is) {
+        Scanner scanner = new Scanner(is);
+        Scanner tokenizer = scanner.useDelimiter("\\A");
+        String str = tokenizer.hasNext() ? tokenizer.next() : "";
+        scanner.close();
+        return str;
+    }
+
+    /**
+     * Send a command to the server for execution and return the server response
+     *
+     * This method serves as an abstraction to send a message to the server and return
+     * exactly what the server sends back.
+     *
+     * @param command The command to execute (Databox format)
+     * @return The server's response, or an empty string if the response could not be retrieved
+     */
+    private String executeServerCommand(String command) {
+        HttpClient client = initClient();
+        HttpPost request = new HttpPost();
+
         try {
-            // Initialize socket connection
-            Socket socket = new Socket(SERVER, SERVERPORT);
+            request.setURI(new URI(API_ENDPOINT_URL));
+            request.setEntity(new StringEntity(command, "UTF-8"));
 
-            // Set up the I/O objects
-            is = socket.getInputStream();
-            os = socket.getOutputStream();
-            isr = new InputStreamReader(is);
-            osw = new OutputStreamWriter(os);
-            br = new BufferedReader(isr);
-            bw = new BufferedWriter(osw);
+            HttpResponse response = client.execute(request);
 
-            return 0;
-        }
-        catch (IOException e) {
-            System.out.println("IO Exception: " + e.toString());
+            if(response.getStatusLine().getStatusCode() != 200) {
+                return ""; // An error of some sort occurred
+            }
+
+            return inputStreamToString(response.getEntity().getContent());
+        } catch (URISyntaxException e) {
             e.printStackTrace();
-            return 1;
-        }
-        catch (Exception e) {
+            return "";
+        } catch (IOException e) {
             e.printStackTrace();
-            return 1;
+            return "";
         }
     }
 
@@ -75,36 +113,15 @@ public class Client {
      * @return 0 on success and 1 on failure
      */
     public int addUser(String username, String password){
-        if(initSocket() == 1)
-            return 1;
-        try {
-            // Write a checkLogin message to the server
-            bw.write(ADDUSER, 0, ADDUSER.length());
-            bw.write(" " + username + " " + password + "\n", 0, username.length() +
-                    password.length() + 3);
-            bw.flush();
+        String command = ADDUSER + " " + username + " " + password + "\n";
+        String response = executeServerCommand(command);
 
-            // Read socket until it's closed to determine response from server
-            bytesRead = br.read(msg, 0, MSG_SIZE);
-            while (bytesRead != -1) {
-                if (new String(msg).trim().equals("FAILURE"))           // credentials failed
-                    return 1;
-                else if (new String(msg).trim().equals("SUCCESS"))      // credentials verified for user
-                    return 0;
-                bytesRead = br.read(msg, 0, MSG_SIZE);
-            }
+        if(response.trim().equals("FAILURE"))
+            return 1;
+        if(response.trim().equals("SUCCESS"))
+            return 0;
 
-            return 1;
-        }
-        catch (IOException e) {
-            System.out.println("IO Exception: " + e.toString());
-            e.printStackTrace();
-            return 1;
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-            return 1;
-        }
+        return 1;
     }
 
     /**
@@ -119,39 +136,17 @@ public class Client {
      * @return 0 on success, 1 on failure
      */
     public int checkLogin(String username, String password) {
-        if (initSocket() == 1)
+        String command = LOGIN + " " + username + " " + password + "\n";
+        String response = executeServerCommand(command);
+
+        if(response.trim().equals("FAILURE"))
             return 0;
-
-        try {
-            // Write a checkLogin message to the server
-            bw.write(LOGIN, 0, LOGIN.length());
-            bw.write(" " + username + " " + password + "\n", 0, username.length() +
-                    password.length() + 3);
-            bw.flush();
-
-            // Read socket until it's closed to determine response from server
-            bytesRead = br.read(msg, 0, MSG_SIZE);
-            while (bytesRead != -1) {
-                if (new String(msg).trim().equals("FAILURE"))           // credentials failed
-                    return 0;
-                else if (new String(msg).trim().equals("SUCCESSUSER"))      // credentials verified for user
-                    return 1;
-                else if (new String(msg).trim().equals("SUCCESSADMIN"))
-                    return 2;
-                bytesRead = br.read(msg, 0, MSG_SIZE);
-            }
-
+        else if(response.trim().equals("SUCCESSUSER"))
             return 1;
-        }
-        catch (IOException e) {
-            System.out.println("IO Exception: " + e.toString());
-            e.printStackTrace();
-            return 1;
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-            return 1;
-        }
+        else if(response.trim().equals("SUCCESSADMIN"))
+            return 2;
+
+        return 0;
     }
 
 }
