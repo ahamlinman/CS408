ea36697a6345a7cfd438bc32a74db9f7ccce95d1
Alex Hamlin <alexh1995@gmail.com>
2015-03-05 10:30:20 -0500

    Updates to package scanning
    Give BeanItems instead of Packages (to make binding to a detail editor
    easier) and show the "Name" field (which might be used to make notes on
    packages).

--- server/packattk-ui/src/main/java/tk/packattk/components/listeners/PackageSelectListener.java
+++ server/packattk-ui/src/main/java/tk/packattk/components/listeners/PackageSelectListener.java
@@ -1,5 +1,9 @@
 package tk.packattk.components.listeners;
 
+import tk.packattk.utils.Package;
+
+import com.vaadin.data.util.BeanItem;
+
 public interface PackageSelectListener {
-	public void packageSelected(tk.packattk.utils.Package selectedPackage);
+	public void packageSelected(BeanItem<Package> selectedPackage);
 }
