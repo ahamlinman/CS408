b4c209d6c69847157281a0f0a1d0b10ea34f7d4c
mtracy01 <matthetracy@gmail.com>
2015-03-08 16:43:52 -0400

    Updated client to populate lists of packages

---
+++ android/Packattk/app/src/main/java/cs408/packattk/Data.java
@@ -0,0 +1,8 @@
+package cs408.packattk;
+
+/**
+ * Created by Matthew on 3/8/2015.
+ */
+public class Data {
+    public static String username="";
+}
--- android/Packattk/app/src/main/java/cs408/packattk/StudentActivity.java
+++ android/Packattk/app/src/main/java/cs408/packattk/StudentActivity.java
@@ -1,15 +1,22 @@
 package cs408.packattk;
 
+import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import java.util.List;
 
 
 public class StudentActivity extends ActionBarActivity {
+
+    private ListView listView;
     private Client client = new Client();
+    private Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,16 +24,25 @@ public class StudentActivity extends ActionBarActivity {
         setContentView(R.layout.activity_student);
 
         Button refreshButton= (Button) findViewById(R.id.refresh_button);
+        listView = (ListView)findViewById(R.id.listView);
         refreshButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 attemptRefresh();
             }
         });
+
+        //Populate the list of packages
+        List<String> packageList = client.getPackages(Data.username);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.activity_list_item,packageList);
+        listView.setAdapter(adapter);
     }
 
     public void attemptRefresh() {
-        client.getPackages("username");
+        List<String> packageList = client.getPackages(Data.username);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.activity_list_item,packageList);
+        listView.setAdapter(adapter);
     }
 
     @Override
